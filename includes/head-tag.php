50389ee3d6dc9e41a3c1c881d8036bf65da32b15
ronald <rordonez@softecdigital.com>
2020-03-02 09:07:29 +0800

    initial commits

---
+++ includes/head-tag.php
@@ -0,0 +1,36 @@
+<title><?php print $PAGE_TITLE;?></title>
+
+<?php if ($CURRENT_PAGE == "Index") { ?>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Paws on my way home" />
+	<meta name="keywords" content="pets, paws" /> 
+
+  	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+  	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+<?php } ?>
+
+
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+<!-- <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> -->
+
+<!-- MAIN STYLESHEET -->
+<link rel="stylesheet" type="text/css" href="./assets/css/main-stylesheet.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/header.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/footer.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/log-in.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/donation.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/sponsor-animal.css">
+<!-- Main javascript -->
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="./assets/js/main.js"></script>
+
+
---
+++ includes/navigation.php
@@ -0,0 +1,100 @@
+<div class="header-container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#"><span class="header-title font-weight-bolder">PawsOnMyWayHome</span></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown header-mega-dropdown bg-color">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Menu
+                    </a>
+                    <div class="dropdown-menu header-mega-dropdown-menu" aria-labelledby="navbarDropdown">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <ul class="header-dropdown-menu">
+                                    <li><a href="./">Home</a></li>
+                                    <li><a href="./about-us.php">About Us</a></li>
+                                    <li><a href="#">Hall Of Fame (2018)</a></li>                            
+                                    <li><a href="#">Events</a></li>  
+                                    <li><span class="small">- No Dog Left Behind</span></li>
+                                    <li><span class="small">- Mixer 2018</span></li>
+                                    <li><span class="small">- 2019 Gold Tournament</span></li>
+                                                            
+                                </ul>
+                            </div>
+                            <div class="col-sm-4">
+                                <ul class="header-dropdown-menu">
+                                    <li><a href="./start-fundraising.php">Fundraising</a></li>
+                                    <li><a href="#">Adoptable Animals</a></li>
+                                    <li><a href="#">How to Adopt</a></li>                            
+                                    <li><a href="#">How can i help?</a></li>                          
+                                    <li><a href="#">Fostering</a></li>
+                                    <li><a href="#">Surrendering you pet</a></li>
+                                    <li><a href="#">Volunteer Sign-Up</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-4">
+                                <ul class="header-dropdown-menu">
+                                    <li><a href="./sponsor-and-animal.php">Sponsor and Animal</a></li>
+                                    <li><a href="#">Warm Your Heart Successes</a></li>
+                                    <li><a href="#">Apply to Adopt Here</a></li>                            
+                                    <li><a href="#">Join our Foster Team!</a></li>                          
+                                    <li><a href="#">Never Forgotten</a></li>
+                                    <li><a href="#">Meet The Board</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./start-fundraising.php">Start Fundraising <span class="sr-only">(current)</span></a>
+                </li>
+
+            </ul>
+            
+
+            <div class="navbar-nav">
+                <li class="nav-item dropdown header-mega-dropdown">
+                    <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Search
+                    </a>
+                    <div class="dropdown-menu header-search-dropdown-menu" aria-labelledby="navbarDropdown">
+                        <div class="header-search-container">
+                            <form class="form-inline my-2 my-lg-0">
+                                <div class ="header-search-text-container">
+                                    <input class="form-control header-search-text" type="search" placeholder="Search" aria-label="Search">
+                                    
+                                </div>
+                                <div class ="header-search-btn-container">
+                                    <button class="btn btn-header-search" type="submit">Search</button>
+                                </div>
+                                <div class="selectdiv ">
+                                    <label>
+                                          <select>
+                                              <option > Please select </option>
+                                              <option selected>Adoption</option>
+                                              <option>Option 2</option>
+                                              <option>Last long option</option>
+                                          </select>
+                                      </label>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+
+                </li> 
+                <a href="log-in.php" class="nav-item nav-link">Log in</a> 
+                <div class="header-login-separator">| </div>
+                <a href="#" class="nav-item nav-link">Sign up</a>
+            </div>
+        </div>
+    </nav>
+    <hr style="margin:0" />
+</div>
